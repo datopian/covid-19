cb0b3865624703c97fa3e994e0bfe72836140f7a
anuveyatsu <anuar.ustayev@gmail.com>
2020-04-11 14:42:49 +0600

    [styles][m]: new font family + custom height utility classes for tailwind.

--- src/dashboard/tailwind.config.js
+++ src/dashboard/tailwind.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  theme: {}
+  theme: {
+    // 👇 this makes Tailwind merge the configuration w/o overriding it.
+    extend: {
+      height: theme => ({
+        "screen-0.8": "calc(100vh * 0.8)",
+        "screen-0.7": "calc(100vh * 0.7)",
+        "screen-0.6": "calc(100vh * 0.6)",
+        "screen-0.5": "50vh",
+        "screen-0.33": "calc(100vh / 3)",
+        "screen-0.25": "calc(100vh / 4)",
+        "screen-0.2": "calc(100vh / 5)",
+      }),
+    },
+  },
+  variants: { display: ["responsive", "hover", "focus"] },
+  plugins: [],
 }
