fd86823eb2c736df93da3b07cd2b0b5c34eb2d30
anuveyatsu <anuar.ustayev@gmail.com>
2020-04-24 10:38:29 +0600

    Title in the navbar should link to `/` page - refs #7

--- src/dashboard/src/Header.js
+++ src/dashboard/src/Header.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Header = () => (
   <nav className="flex items-center justify-between flex-wrap bg-white py-6 px-6 md:px-16 shadow-sm">
     <div className="flex items-center flex-shrink-0 text-gray-600 mr-6">
-      <span className="text-xl tracking-tight">COVID-19 DASHBOARD</span>
+      <a href="/">
+        <span className="text-xl tracking-tight">COVID-19 DASHBOARD</span>
+      </a>
     </div>
     <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div className="text-sm lg:flex-grow">
